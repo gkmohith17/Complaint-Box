212fe54a603088ccca1c991a861c3cac26312c67
Spoorthinbhat <spoorthiullal4@gmail.com>
2024-07-11 23:25:38 +0530

    backend added with login and signup functionality

---
+++ backend/models/Complaint.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const ComplaintSchema = new mongoose.Schema({
+  category: { type: String, required: true },
+  othercategory: { type: String },
+  state: { type: String, required: true },
+  district: { type: String, required: true },
+  pincode: { type: String, required: true },
+  comment: { type: String, required: true },
+  photo: { type: String },
+  video: { type: String }
+}, { collection: 'Complaints' }); // Explicitly specify the collection name if needed
+
+module.exports = mongoose.model('Complaint', ComplaintSchema);
