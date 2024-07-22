f9d8cf00cb2c46047c4afc6d7327399dc6a6c912
gkmohith17 <gkmohith17@gmail.com>
2024-07-22 16:34:59 +0530

    Added Crypto files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,8 +5,11 @@ module.exports = withWebpack({
   webpack: (config) => {
     config.resolve.fallback = {
       crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
       // Add other fallbacks if necessary
     };
     return config;
   },
 });
+
